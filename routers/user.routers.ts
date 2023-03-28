85127e5f9e27309aa045579037b3ce702d0118ec
Rafal Nalewajek <rafal.nalewajek@gmail.com>
2023-03-28 07:14:43 +0200

    Removed post method.

--- routers/user.routers.ts
+++ routers/user.routers.ts
@@ -1,20 +1,13 @@
 import { Router } from 'express';
 import { UserRecord } from '../records/user.record';
 
-export const userRoters = Router()
+export const userRouters = Router()
   .get('/:id', async (req, res) => {
     const id = req.params.id;
 
     const user = await UserRecord.getOne(id);
 
     res.json(user);
-  })
-
-  .post('/', async (req, res) => {
-    const body = req.body;
-    body.postUserAdded = 'added';
-
-    res.json(body);
   })
 
   .put('/:id', async (req, res) => {
